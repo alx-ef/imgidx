68855b3895d37e6f44a1f5c8ab3c8200570a546b
Alexander Efremov <a.s.efremov@gmail.com>
2022-08-29 21:52:16 +0300

    Add Vector.Distance function

--- embedders/embedders.go
+++ embedders/embedders.go
@@ -11,6 +11,16 @@ var (
 	ErrEmptyImage = errors.New("the image is nil or empty")
 )
 
+func (v1 Vector) Distance(v2 Vector) float64 {
+	// same as for the kdtree.Point type
+	var sum float64
+	for dim := range v1 {
+		d := v1[dim] - v2[dim]
+		sum += d * d
+	}
+	return sum
+}
+
 type ImageEmbedder interface {
 	Img2Vec(image.Image) (Vector, error)
 	// Dims returns the number of dimensions of vectors produced by the ImageEmbedder
