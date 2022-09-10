f8f1bca061ead9f44405501170bc071fe5b21d3d
Alexander Efremov <a.s.efremov@gmail.com>
2022-09-10 10:50:38 +0300

    clean redundant comments

--- perf_test.go
+++ perf_test.go
@@ -8,12 +8,6 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// goos: darwin, goarch: arm64
-// Before optimisation
-// BenchmarkComposite_Img2Vec_Jpeg-10           134           8808894 ns/op         1424701 B/op     443733 allocs/op
-// After optimisation
-// BenchmarkComposite_Img2Vec_Jpeg-10           480           2379291 ns/op          602853 B/op         11 allocs/op
-// ok      github.com/alef-ru/imgidx       2.151s
 func BenchmarkComposite_Img2Vec_Jpeg(b *testing.B) {
 	idx, err := NewCompositeIndex(8, 8)
 	assert.NoError(b, err, "Failed to create index")
